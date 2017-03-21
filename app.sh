88180d066902b9ce2902eea0ddc2e5cf6f7caa21
Katherine Crowson <crowsonkb@gmail.com>
2017-03-21 12:12:40 -0700

    Use 4-space indent in shellscript

--- app.sh
+++ app.sh
@@ -3,27 +3,27 @@
 set -e
 
 wrap () {
-  COLUMNS=$(tput cols || echo 80)
-  echo "$@" | fmt -w $((COLUMNS - 5))
+    COLUMNS=$(tput cols || echo 80)
+    echo "$@" | fmt -w $((COLUMNS - 5))
 }
 
 cd "${0%/*}"
 
 if [[ ! -d venv ]]; then
-  echo "First run; creating virtual environment..."
-  wrap "Using ${PYTHON_EXE:="$(which python3)"} as the Python interpreter. Set the PYTHON_EXE variable to use a different Python interpreter."
-  "$PYTHON_EXE" -m venv venv
-  source venv/bin/activate
-  pip install -U pip setuptools wheel
-  pip install -Ur requirements.txt
-  echo
+    echo "First run; creating virtual environment..."
+    wrap "Using ${PYTHON_EXE:="$(which python3)"} as the Python interpreter. Set the PYTHON_EXE variable to use a different Python interpreter."
+    "$PYTHON_EXE" -m venv venv
+    source venv/bin/activate
+    pip install -U pip setuptools wheel
+    pip install -Ur requirements.txt
+    echo
 else
-  source venv/bin/activate
+    source venv/bin/activate
 fi
 
 echo "Starting uWSGI..."
 if [[ ! -f uwsgi.ini ]]; then
-  cp uwsgi_example.ini uwsgi.ini
+    cp uwsgi_example.ini uwsgi.ini
 fi
 
 uwsgi --ini uwsgi.ini "$@"
